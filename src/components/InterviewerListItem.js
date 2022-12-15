d11b1e99a7863d54601be17981944b5ad01c6622
Anshaal Hussain <anshaalhussain@gmail.com>
2022-12-15 07:45:24 -0500

    create InterviewerListItem component and styling

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classNames from "classnames";
+import "./InterviewerListItem.scss";
+
+const InterviewerListItem = (props) => {
+  const InterviewClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+  return (
+    <li
+      onClick={() => props.setInterviewer(props.id)}
+      className={InterviewClass}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt="Sylvia Palmer"
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
